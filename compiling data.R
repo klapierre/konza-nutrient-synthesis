ebc42cc537a4c1a22d63394f4f747966243ee4a2
klapierre <lapierre.kimberly@gmail.com>
2017-02-24 14:01:49 -0600

    Added invert removal datasets.

--- compiling data.R
+++ compiling data.R
@@ -1,4 +1,11 @@
+library(tidyr)
+library(dplyr)
+
+#meghan's:
 setwd("~/Dropbox/Konza Nutrient Synthesis")
+
+#kim's:
+setwd('C:\\Users\\Kim\\Dropbox\\konza projects\\Konza Nutrient Synthesis')
 
 #treatments
 bgp_trt<-read.csv("BGPE_ANPP_1986-2015.csv")%>%
@@ -13,6 +20,10 @@ sp_pplots<-read.csv("pplots_spp comp_2002-2015.csv")
 
 sp_bgp<-read.csv("BGPE_spp comp_1986-2012.csv")%>%
   group_by()
+
+sp_invert <- read.csv('Vert Invert_spp comp_2009-2016.csv')%>%
+  mutate(project_name='invert', calendar_year=year, plot_id=plot, treatment=trt_other_name, genus_species=taxa, abundance=cover)%>%
+  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)
 
 
 ###anpp data
@@ -30,3 +41,14 @@ anpp_bgp_cleaned<-anpp_bgp%>%
   summarize(anpp=mean(anpp))
          
 anpp_pplots<-read.csv("pplots_anpp_2002-2015.csv")
+
+anpp_invert <- read.csv('Vert Invert_anpp_2009-2015.csv')%>%
+  mutate(project_name='invert', calendar_year=date, plot_id=plot, treatment=trt_other_name)%>%
+  select(project_name, calendar_year, plot_id, treatment, anpp)
+
+
+
+
+
+
+
