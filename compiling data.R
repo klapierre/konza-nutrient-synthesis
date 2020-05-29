f11d0d389b5b4b3d5d35c73af6f7c958a106761a
klapierre <lapierre.kimberly@gmail.com>
2020-05-29 13:07:01 -0400

    Updating all data to be the latest version from Konza website. Cleaning and combining.

--- compiling data.R
+++ compiling data.R
@@ -1,175 +1,272 @@
 library(tidyverse)
 
-#meghan's:
-setwd("~/Dropbox/Konza Nutrient Synthesis")
-
-#kim's:
-setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\konza projects\\Konza Nutrient Synthesis\\Threshold project\\data\\2018 analysis')
-
-#konza spp list
-spp <- read.csv('konza_spplist.csv')
-nutnetSp <- read.csv('nutnet_spplist.csv')%>%
-  mutate(sppnum=SppNum)
-viSp <- read.csv('invert_spplist.csv')%>%
-  mutate(sppnum=SppNum)
+###setting working directories----------
+setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\konza projects\\Konza Nutrient Synthesis\\data') #kim's laptop
+setwd("~/Dropbox/Konza Nutrient Synthesis") #meghan's
 
 
-#treatments
-bgp_trt <- read.csv("belowground_plots_anpp_1989-2015.csv")%>%
+###functions----------
+#not in function
+`%notin%` <- Negate(`%in%`)
+
+
+
+###plant species composition data----------
+##konza spp lists
+spp <- read.csv('species_list\\PPS011_new KNZ spp list.csv')
+
+##treatments - for experiments where treatments designations are not in the species composition file
+bgp_trt <- read.csv("treatments\\belowground_plots_anpp_1989-2015.csv")%>%
   filter(MOW!="m")%>%
-  mutate(treatment=paste(BURN, MOW, NUTRIENT, sep="_"),
+  mutate(treat_other_name=paste(BURN, MOW, NUTRIENT, sep="_"),
          plot_id=PLOT)%>%
-  select(plot_id,treatment)%>%
-  unique()%>%
-  filter(treatment!="__")
-
-change_trt <- read.csv("ChANGE_treatments.csv")%>%
-  mutate(plot_id=plot, treatment=as.factor(N))%>%
-  select(plot_id, treatment)
-
-nutnet_trt <- read.csv('KNZ_NutNet_trt.csv')%>%
+  filter(treat_other_name!="__")%>%
+  mutate(project_name=ifelse(treat_other_name %in% c('u_u_n','u_u_c','u_u_p','u_u_b'), 'BGP unburned', 'BGP burned'),
+         treatment=ifelse(treat_other_name %in% c('u_u_c','b_u_c'), 'control', treat_other_name))%>%
+  select(project_name,plot_id,treatment)%>%
+  unique()
+change_trt <- read.csv("treatments\\ChANGE_treatments.csv")%>%
+  mutate(plot_id=plot, treatment=ifelse(N==0, 'control', as.factor(N)), 
+         project_name='ChANGE')%>%
+  select(project_name,plot_id, treatment)
+nutnet_trt <- read.csv('treatments\\KNZ_NutNet_trt.csv')%>%
   mutate(project_name='nutnet')%>%
-  select(project_name, plot, treat_other_name)
-
-ghostfire_trt <- read.csv('ghost fire_trt.csv')%>%
-  mutate(plot_id=paste(Burn.Trt,Plot, sep='_'))%>%
-  mutate(treatment=paste(Litter,Nutrient))%>%
-  select(plot_id, treatment)
-
-
-###species data
-sp_pplots <- read.csv("pplots_sppcomp_2002-2018.csv")%>%
-  mutate(project_name="pplots")%>%
-  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)
-
-sp_bgp <- read.csv("belowground_plots_sppcomp_1989-2017.csv")%>%
-  mutate(project_name="BGP", 
-         genus_species=paste(Ab_genus, Ab_species, sep=" "),
+  rename(treatment=treat_other_name, plot_id=plot)%>%
+  select(project_name, plot_id, treatment)
+invert_trt <- read.csv('treatments\\invert_trt.csv')%>%
+  mutate(project_name='invert',
+         treat_other_name=paste(NPK,insecticide,exclose,sep='_'))%>%
+  rename(plot_id=plot)%>%
+  mutate(treatment=ifelse(treat_other_name=='x_x_x', 'control', treat_other_name))%>%
+  select(project_name, plot_id, treatment)
+ghostfire_trt <- read.csv('treatments\\ghost fire_trt.csv')%>%
+  mutate(project_name=ifelse(Burn.Trt=='Annual', 'GF Burned', 'GF Unburned'), 
          plot_id=Plot,
-         calendar_year=RecYear,
-         abundance=ifelse(CoverClass==1, 1, ifelse(CoverClass==2, 2.5, ifelse(CoverClass==3, 10, ifelse(CoverClass==4, 37.5, ifelse(CoverClass==5, 62.5, ifelse(CoverClass==6, 85, 97.5)))))))%>%
-  group_by(project_name, genus_species, plot_id, calendar_year)%>%
-  summarize(abundance=mean(abundance))%>%
-  ungroup()%>%
-  #check with john about why some data has no spp id, genus, or species
-  filter(genus_species!=' ')%>%
-  right_join(bgp_trt)%>%
-  mutate(project_name=ifelse(treatment=='u_u_n'|treatment=='u_u_c'|treatment=='u_u_p'|treatment=='u_u_b', 'BGP unburned', 'BGP burned'))%>%
-  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)
+         treat_other_name=paste(Litter,Nutrient, sep='_'),
+         treatment=ifelse(treat_other_name=='A_C'&project_name=='GF Burned', 'control', ifelse(treat_other_name=='P_C'&project_name=='GF Unburned', 'control', treat_other_name)))%>%
+  select(project_name,plot_id, treatment)
+ukulinga_trt <- read.csv('treatments\\ukulinga_trt.csv')%>%
+  mutate(project_name=ifelse(site=='1D', 'ukulinga_annual', ifelse(site=='4F', 'ukulinga_four', 'ukulinga_unburned')),
+         treatment=ifelse(fert==1, 'control', 'N'))
 
-sp_change <- read.csv("ChANGE_sppcomp_2013-2018.csv")%>%
-  mutate(project_name='ChANGE',
-         calendar_year=Year,
-         plot_id=as.numeric(Plot), 
-         genus_species=Species)%>%
-  mutate(abundance=pmax(June,August))%>%
-  select(project_name, calendar_year, plot_id, genus_species, abundance)%>%
-  right_join(change_trt)
-
-sp_invert <- read.csv('VI_sppcomp_2008-2019.csv')%>%
-  mutate(project_name='invert', calendar_year=year, plot_id=plot, treatment=paste(NPK, exclose, insecticide, sep='_'), abundance=rel_cover, spnum=sppnum)%>%
-  left_join(viSp)%>%
+#species comp data with species numbers
+sp_invert <- read.csv('species_comp\\VIR011_invertRemoval.csv')%>%
+  mutate(project_name='invert')%>%
+  rename(calendar_year=RecYear, plot_id=Plot)%>%
+  mutate(code=ifelse(SpeCode %in% c(900, 957), 517, ifelse(SpeCode==795, 246, ifelse(SpeCode==825, 518, ifelse(SpeCode==960, 158, ifelse(SpeCode==956, 152, ifelse(SpeCode==251, 16, SpeCode)))))))%>%
+  left_join(spp)%>%
+  left_join(invert_trt)%>%
   mutate(genus_species=paste(genus, species))%>%
-  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)%>%
+  select(project_name, calendar_year, plot_id, treatment, genus_species, Cover)%>%
   filter(genus_species!='bare ground')%>%
-  filter(calendar_year<2019) #cessation begins
+  filter(calendar_year<2019)%>% #cessation begins
+  group_by(project_name, calendar_year, plot_id, treatment, genus_species)%>%
+  summarise(abundance=max(Cover))%>%
+  ungroup()
 
-sp_nutnet <- read.csv('nutnet_sppcomp_2007-2019.csv')%>%
-  select(-date, -note_cover)%>%
-  spread(key=season, value=cover, fill=0)%>%
-  group_by(year, plot, taxa)%>%
-  mutate(cover=pmax(fall,spring))%>%
-  ungroup()%>%
+sp_nutnet <- read.csv('species_comp\\NUT011_nutnet.csv')%>%
+  mutate(project_name="nutnet")%>%
+  rename(calendar_year=RecYear, plot_id=Plot)%>%
+  mutate(code=ifelse(Sppnum %in% c(900, 957), 517, ifelse(Sppnum==795, 246, ifelse(Sppnum %in% c(951,825), 518, ifelse(Sppnum==960, 158, ifelse(Sppnum==956, 152, ifelse(Sppnum==251, 16, ifelse(Sppnum==267, 52, ifelse(Sppnum==726, 246, Sppnum)))))))))%>%
+  left_join(spp)%>%
   left_join(nutnet_trt)%>%
-  select(-taxa)%>%
-  left_join(nutnetSp)%>%
-  mutate(genus_species=ifelse(taxa=='Muhlenbergia cuspidata', 'Muhlenbergia racemosa', ifelse(taxa=='Euphorbia serpens', 'Euphorbia nutans', as.character(taxa))))%>%
-  mutate(project_name='nutnet', calendar_year=year, plot_id=plot, treatment=treat_other_name, abundance=cover)%>%
-  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)%>%
-  filter(genus_species!='bare ground', treatment!='fence'&treatment!='NPK_fence')%>%
-  filter(calendar_year<2019) #for consistency with other datasets
+  filter(Taxa!='bare ground'&Taxa!='litter')%>%
+  mutate(genus_species=paste(genus, species))%>%
+  group_by(project_name, calendar_year, plot_id, treatment, genus_species)%>%
+  summarise(abundance=max(Cover))%>%
+  ungroup()
 
-sp_ghostfire<-read.csv("ghost fire_spp comp_2014-2018.csv")%>%
-  mutate(genus_species=paste(genus,species), project_name=ifelse(Burn.Trt=='Annual', 'GF Burned', 'GF Unburned'), calendar_year=Year, blockplot=paste(Block,Plot, sep=''), plot_id=paste(Burn.Trt, blockplot, sep='_'))%>%
-  left_join(ghostfire_trt)%>%
-  group_by(project_name, calendar_year, treatment, genus_species, plot_id)%>%
-  summarise(abundance=max(cover))%>%
+sp_bgp <- read.csv("species_comp\\BGPVC_belowgroundPlots.csv")%>%
+  rename(calendar_year=RecYear, code=SpeciesCode, plot_id=Plot)%>%
+  left_join(spp)%>%
+  mutate(genus_species=paste(genus,species,sep='_'))%>%
+  mutate(abundance=ifelse(CoverClass==1, 1, ifelse(CoverClass==2, 2.5, ifelse(CoverClass==3, 10, ifelse(CoverClass==4, 37.5, ifelse(CoverClass==5, 62.5, ifelse(CoverClass==6, 85, 97.5)))))))%>%
+  group_by(genus_species, plot_id, calendar_year)%>%
+  summarize(abundance=mean(abundance))%>% #average over subplots
   ungroup()%>%
-  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)
+  right_join(bgp_trt)%>%
+  select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)%>%
+  filter(calendar_year<2017) #remove data after cessation of treatmetns
 
-# sp_restoration <- read.csv('restoration plots_spp comp_1999-2012.csv')%>%
-#   filter(DEPTH==1)%>%
-#   select(-OBS, -BLOCK, -DEPTH, -NUTRIENT, -RESTORE_YR, -RESIN_NO3, -H, -R, -ANPP, -WPTRT)%>%
-#   gather(key=genus_species, value=cover, ANGE:TRRE3)%>%
-#   mutate(cover=as.numeric(cover))%>%
-#   filter(cover>0)%>%
-#   group_by(PLOT, TRTCOMB, YEAR, genus_species)%>%
-#   summarise(abundance=mean(cover))%>%
-#   ungroup()%>%
-#   mutate(project_name='restoration', calendar_year=YEAR, plot_id=PLOT, treatment=TRTCOMB)%>%
-#   select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)
-# 
-# sp_ukulinga_all <- read.csv('KNZ UK_spp comp_2005-2010.csv')%>%
-#   unite(plot_label, plot, fert, sep='_', remove=F)
-# 
-# ukulinga_plots <- sp_ukulinga_all%>%
-#   select(plot_label)%>%
-#   unique()%>%
-#   arrange(plot_label)%>%
-#   mutate(plot_id=seq(1,6, by=1))
-# 
-# sp_ukulinga <- sp_ukulinga_all%>%
-#   merge(ukulinga_plots, by=c('plot_label'))%>%
-#   gather(key=covyear, value=cover, cov2005:cov2010)%>%
-#   filter(cover>0)%>%
-#   separate(covyear, c('cov', 'year'), sep='v')%>%
-#   group_by(site, fert, plot_id, subplot, spnum, year)%>%
-#   summarise(cover=mean(cover))%>%
-#   ungroup()%>%
-#   group_by(site, fert, plot_id, spnum, year)%>%
-#   summarise(cover=mean(cover))%>%
-#   ungroup()%>%
-#   merge(knz_spplist, by=c('spnum'))%>%
-#   mutate(treatment=ifelse(fert==1, 'control', 'N'), project_name=ifelse(site=='1D', 'ukulinga annual', ifelse(site=='4F', 'ukulinga four', 'ukulinga unburned')), abundance=cover, calendar_year=year)%>%
-#   select(project_name, calendar_year, plot_id, treatment, genus_species, abundance)
+sp_ghostfire <- read.csv("species_comp\\GFE011_ghostFire.csv")%>%
+  mutate(plot_id=paste(Block,Plot,sep=''))%>%
+  rename(code=Spnum, calendar_year=RecYear)%>%
+  left_join(spp)%>%
+  left_join(ghostfire_trt)%>%
+  mutate(genus_species=paste(genus,species,sep='_'))%>%
+  group_by(project_name, calendar_year, treatment, genus_species, plot_id)%>%
+  summarise(abundance=max(Cover))%>%
+  ungroup()
+
+#without species numbers
+sp_pplots <- read.csv("species_comp\\PPL011_pplots.csv")%>%
+  mutate(project_name="pplots", 
+         genus_species=paste(Genus, Species, sep='_'),
+         treatment=as.factor(ifelse(Treatment=='n1p0', 'control', as.character(Treatment))))%>%
+  rename(calendar_year=RecYear, plot_id=PlotID)%>%
+  group_by(project_name, calendar_year, plot_id, treatment, genus_species)%>%
+  summarise(abundance=max(Abundance))%>%
+  ungroup()
+
+sp_change <- read.csv("species_comp\\KNZ_change_sppComp.csv")%>%
+  rename(calendar_year=year, plot_id=plot, genus_species=species)%>%
+  left_join(change_trt)%>%
+  group_by(project_name, calendar_year, plot_id, treatment, genus_species)%>%
+  summarise(abundance=max(cover))%>%
+  ungroup()
+
+sp_ukulinga <- read.csv('species_comp\\KNZ_ukulinga_sppComp.csv')%>%
+  left_join(ukulinga_trt)%>%
+  gather(key='year', value='cover', cov2005:cov2010)%>%
+  separate(col=year, into=c('trash','calendar_year'), sep='v')%>%
+  rename(code=spnum)%>%
+  left_join(spp)%>%
+  mutate(calendar_year=as.numeric(calendar_year),
+         genus_species=paste(genus,species,sep='_'))%>%
+  group_by(project_name, calendar_year, treatment, genus_species, plot, subplot, ssubplot)%>%
+  summarise(max_cov=max(cover))%>%
+  ungroup()%>%
+  group_by(project_name, calendar_year, treatment, genus_species, plot, subplot)%>%
+  summarise(abundance=mean(max_cov))%>% #averages across sub-subplots
+  ungroup()%>%
+  mutate(plot_id=paste(plot,subplot,sep='_'))%>%
+  select(project_name, calendar_year, treatment, genus_species, abundance, plot_id)
 
 
-###merging
+
+###merging species composition data----------
 #species data
 sp_all <- sp_pplots%>%
   rbind(sp_bgp)%>%
   rbind(sp_change)%>%
   rbind(sp_invert)%>%
   rbind(sp_nutnet)%>%
-  rbind(sp_ghostfire)
-  # rbind(sp_restoration)%>%
-  # rbind(sp_ukulinga)
-
-  
-
-# write.csv(sp_all, 'Konza_nutrient synthesis_spp comp_04142020.csv', row.names=F)
-
-# #anpp data
-# anpp_all <- anpp_pplots%>%
-#   rbind(anpp_bgp)%>%
-# #  rbind(anpp_change)%>%
-#   rbind(anpp_invert)%>%
-#   rbind(anpp_nutnet)%>%
-#   rbind(anpp_ghostfire)%>%
-#   rbind(anpp_restoration)
+  rbind(sp_ghostfire)%>%
+  rbind(sp_ukulinga)%>%
+  filter(abundance>0)
 
 
-#grasshopper data
-grasshopper <- read.csv('konza_grasshoppers.csv')%>%
-  filter(WATERSHED %in% c('000b', '001d', '001D', '002c', '002C', '002d', '002D', '004b', '004B', '004d', '004D', '004f', '004F', '020b', '020B', '0spb', '0SpB'), TOTAL!='')%>%
+###calculating relative cover----------
+totCov <- sp_all%>%
+  group_by(project_name, calendar_year, treatment, plot_id)%>%
+  summarise(tot_cover=sum(abundance))%>%
+  ungroup()
+
+relCov <- sp_all%>%
+  rename(cov=abundance)%>%
+  left_join(totCov)%>%
+  mutate(abundance=cov/tot_cover)
+
+# write.csv(relCov, 'Konza_nutrient synthesis_spp comp_05282020.csv', row.names=F)
+
+
+
+###consumer data----------
+#grasshoppers
+grasshopper <- read.csv('drivers\\CGR022_grasshoppers.csv')%>%
+  filter(!is.na(TOTAL), SPECIES %notin% c('Gryllidae spp.','Oecanthinae spp.','Tettigoniidae spp.','Orchelimum spp.','Neoconocephalus ensiger','Neoconocephalus robustus','Scudderia texensis'))%>% #remove non-Acrididae species, which were not counted in all years
+  group_by(RECYEAR,RECMONTH,RECDAY,WATERSHED,SOILTYPE,REPSITE)%>%
+  summarise(total_count=sum(TOTAL))%>% #sum of grasshoppers across all species for each rep
+  ungroup()%>%
+  filter(WATERSHED %notin% c('010d','0sub','n00b','n01a','n01b','n04a','n04d','n20a','n20b'))%>% #drop watersheds not in our experimental framework (e.g., 10 yr burn, grazed)
+  filter(WATERSHED %notin% c('000b','004d','004g'))%>% #drop watersheds with too little data
+  group_by(RECYEAR, RECMONTH, RECDAY, WATERSHED, SOILTYPE)%>%
+  summarise(trans_count=sum(total_count))%>% #sum across reps within a transect
+  ungroup()%>%
+  group_by(RECYEAR, WATERSHED, SOILTYPE)%>%
+  summarise(max_count=max(trans_count))%>% #max across two sampling time points (early vs late season)
+  ungroup()%>%
   group_by(RECYEAR, WATERSHED)%>%
-  summarize(count=mean(as.numeric(TOTAL)))%>%
+  summarise(ws_count=mean(max_count))%>% #average over transects within a watershed
+  ungroup()%>%
+  mutate(burn=ifelse(WATERSHED %in% c('001d','0spb'), 'annual', ifelse(WATERSHED %in% c('002c','002d'), 'two', ifelse(WATERSHED %in% c('004b','004f'), 'four', 'twenty'))))%>%
+  group_by(RECYEAR, burn)%>%
+  summarise(burn_count=mean(ws_count))%>% #average by burn treatment
   ungroup()%>%
   group_by(RECYEAR)%>%
-  summarize(grasshopper_count=mean(count))%>%
+  summarise(grasshopper=mean(burn_count))%>% #average across all burn types
   ungroup()%>%
-  rename(year=RECYEAR)
+  rename(calendar_year=RECYEAR)
 
-#write.csv(grasshopper, 'grasshopper_yearly_mean.csv', row.names=F)
+# ggplot(data=grasshopper, aes(x=calendar_year, y=grasshopper)) +
+#   geom_point() + geom_smooth(method='lm', se=F)
+
+#small mammals
+mammal_early <- read.csv('drivers\\CSM01_small mammals_1981-2013.csv')%>%
+  separate(col=WATERSHED.LINE, into=c('WATERSHED','LINE'), sep='-')%>%
+  filter(WATERSHED %in% c('001D','004B','004F','020B'))%>%  #drop watersheds not in our experimental framework (e.g., grazed)
+  filter(SEASON=='AU')%>% #only keep fall sampling dates for comparability to newest records
+  mutate(count=Pm+Rmeg+Sh+Bh+Rmon+St+Mo+Pl+Ch+Mm+Nf+Sc+Zh+Cp)%>% #calculate sum across species
+  group_by(RECYEAR, WATERSHED, LINE)%>%
+  summarise(trans_count=sum(count))%>% #sum across four sampling dates
+  ungroup()%>%
+  group_by(RECYEAR, WATERSHED)%>%
+  summarise(ws_count=mean(trans_count))%>% #mean across transects within watershed
+  ungroup()%>%
+  mutate(burn=ifelse(WATERSHED %in% c('001D'), 'annual', ifelse(WATERSHED %in% c('004B','004F'), 'four', 'twenty')))%>%
+  group_by(RECYEAR, burn)%>%
+  summarise(burn_count=mean(ws_count))%>% #mean by burn treatment
+  ungroup()%>%
+  group_by(RECYEAR)%>%
+  summarise(mammal=mean(burn_count))%>% #mean across all burn types
+  ungroup()%>%
+  rename(calendar_year=RECYEAR)
+  
+mammal_late <- read.csv('drivers\\CSM08_small mammals_2016-2019.csv')%>%
+  filter(Treatment %in% c('1D','4B','4F','20B'))%>%  #drop watersheds not in our experimental framework (e.g., grazed)
+  filter(Recapture.Status..Y.N.=='N')%>% #filter out recaptures
+  group_by(Year, Treatment, Transect)%>%
+  summarise(trans_count=length(Species))%>% #sum across four sampling dates
+  ungroup()%>%
+  group_by(Year, Treatment)%>%
+  summarise(ws_count=mean(trans_count))%>% #mean across transects within watershed
+  ungroup()%>%
+  mutate(burn=ifelse(Treatment %in% c('1D'), 'annual', ifelse(Treatment %in% c('4B','4F'), 'four', 'twenty')))%>%
+  group_by(Year, burn)%>%
+  summarise(burn_count=mean(ws_count))%>% #mean by burn treatment
+  ungroup()%>%
+  group_by(Year)%>%
+  summarise(mammal=mean(burn_count))%>% #mean across all burn types
+  ungroup()%>%
+  rename(calendar_year=Year)
+
+mammal <- mammal_early%>%
+  rbind(mammal_late)
+
+# ggplot(data=mammal, aes(x=calendar_year, y=mammal)) +
+#   geom_point() + geom_smooth(method='lm', se=F)
 
 
+#weather
+precip <- read.csv('drivers\\AWE012_weather.csv')%>%
+  mutate(month=paste('a',RECMONTH, sep=''))%>%
+  group_by(RECYEAR, month)%>%
+  summarise(precip=sum(DPPT))%>%
+  ungroup()%>%
+  spread(key=month, value=precip, fill=0)%>%
+  mutate(grow_precip=(a5+a6+a7+a8)/10,
+         year_precip=(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12)/10)%>%
+  rename(calendar_year=RECYEAR)%>%
+  select(calendar_year, grow_precip, year_precip)
+
+growTemp <- read.csv('drivers\\AWE012_weather.csv')%>%
+  mutate(temp=as.numeric(TAVE))%>%
+  filter(RECMONTH %in% c(5,6,7,8))%>%
+  group_by(RECYEAR)%>%
+  summarise(grow_temp=mean(TAVE))%>%
+  ungroup()%>%
+  rename(calendar_year=RECYEAR)
+yearTemp <- read.csv('drivers\\AWE012_weather.csv')%>%
+  group_by(RECYEAR)%>%
+  summarise(year_temp=mean(TAVE))%>%
+  ungroup()%>%
+  rename(calendar_year=RECYEAR)
+
+#combine drivers
+drivers <- precip%>%
+  full_join(growTemp)%>%
+  full_join(yearTemp)%>%
+  full_join(grasshopper)%>%
+  full_join(mammal)
+
+# write.csv(drivers, 'Konza_nutrient synthesis_drivers_05292020.csv', row.names=F)
